da861ab6401a7ca38d447ca254712727877ca619
TCreopargh <tcreopargh@outlook.com>
2020-10-04 19:08:58 +0800

    optimization

--- src/main/java/xyz/tcreopargh/pouchofunknown/PouchOfUnknownMod.java
+++ src/main/java/xyz/tcreopargh/pouchofunknown/PouchOfUnknownMod.java
@@ -1,14 +1,9 @@
 package xyz.tcreopargh.pouchofunknown;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(
         modid = PouchOfUnknownMod.MODID,
