15509c52aba17def6fc60c8059f14934576c9ea3
TCreopargh <tcreopargh@outlook.com>
2020-10-04 20:59:14 +0800

    fix

--- src/main/java/xyz/tcreopargh/pouchofunknown/PouchConfig.java
+++ src/main/java/xyz/tcreopargh/pouchofunknown/PouchConfig.java
@@ -1,10 +1,6 @@
 package xyz.tcreopargh.pouchofunknown;
 
 import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(
         modid = PouchOfUnknownMod.MODID
